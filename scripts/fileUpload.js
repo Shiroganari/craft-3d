cc55b721066842c05c296dfbc8f2c2480a20bcf7
Shiroganari <shiroganari@gmail.com>
2021-11-07 20:07:17 +0700

    Update upload-progress component

--- scripts/fileUpload.js
+++ scripts/fileUpload.js
@@ -5,6 +5,8 @@ const infoSection = document.getElementById('cost-calculation-info');
 const progressBarItems = document.getElementsByClassName('progress-bar__item');
 const uploadButton = document.getElementById('upload-button');
 const uploadProgress = document.getElementById('upload-progress');
+const uploadProgressWrapper = document.getElementById('upload-progress__wrapper');
+const uploadProgressFill = document.getElementById('upload-progress__fill');
 const dropZone = document.getElementById('drop-zone');
 const fileInput = document.querySelector('.drop-zone__input');
 
@@ -45,17 +47,17 @@ dropZone.addEventListener('drop', (e) => {
 function uploadFile(file) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "upload.php");
+    uploadProgress.style.display = 'flex';
     xhr.upload.addEventListener("progress", ({loaded, total}) => {
         let fileLoaded = Math.floor((loaded / total) * 100);
         let fileTotal = Math.floor(total / 1000);
         let fileSize;
         (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
 
-        let progressHTML = `<div class="upload-title">Загрузка файла</div>
-                            <div class="upload-progress" id="upload-progress">
-                                <div class="upload-progress__item" style="width: ${fileLoaded}%; display='flex'">${fileLoaded}%</div>
-                            </div>`;
-        uploadSection.innerHTML = progressHTML;
+        uploadProgressFill.style.width = fileLoaded + '%';
+        if (fileLoaded > 10) {
+            uploadProgressFill.innerHTML = fileLoaded + '%';
+        }
 
         if (loaded == total) {
             uploadSection.classList.remove('active');
